4d7245a043dbe64d84614ff922955bcad9d67a91
Jzulu22x <juanzr1015@gmail.com>
2024-07-16 06:57:40 -0500

    Actualizacion

---
+++ C# Ruta Avanzada/W3_POO_2/Models/Estudiante.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace W3_POO_2.Models;
+    public class Estudiante
+    {
+        
+    }
